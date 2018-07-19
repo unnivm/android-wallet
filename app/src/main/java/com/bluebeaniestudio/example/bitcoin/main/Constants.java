7dbd031709c481dc122c92e18eb0d82d8711f1a8
unknown <unnivm@gmail.com>
2018-07-19 12:42:45 +0530

    android-wallet project files

---
+++ app/src/main/java/com/bluebeaniestudio/example/bitcoin/main/Constants.java
@@ -0,0 +1,13 @@
+package com.bluebeaniestudio.example.bitcoin.main;
+
+/**
+ * Created by Unni Mana on 18/07/2018.
+ */
+
+public class Constants {
+    /** wallet name **/
+    public static final String WALLET_NAME = "my_wallet";
+
+    /** check for testnet or mainnet **/
+    public static boolean IS_PRODUCTION = false;
+}
